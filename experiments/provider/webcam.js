06d28f6a1b79e5b72a84198364b7fcf085c59d4f
Aneesh Durg <aneeshdurg17@gmail.com>
2020-12-20 20:03:19 -0800

    Add webcam provider

---
+++ experiments/provider/webcam.js
@@ -0,0 +1,36 @@
+class Webcam extends Provider {
+    async setup() {
+        this.setup_ctx();
+        this.video = document.createElement("video");
+
+        const constraints = {
+            video: {
+                mandatory: {
+                    maxWidth:  this.dimensions[0],
+                    maxHeight: this.dimensions[1]
+                }
+            }
+        }
+
+        await new Promise((r) => {
+            navigator.getUserMedia(
+                constraints,
+                (stream) => {
+                    this.video.srcObject = stream;
+                    this.video.play();
+                    r();
+                },
+                function(){}
+            );
+        });
+    }
+
+    tick() {
+        if (!this.video.paused && !this.video.ended)
+            this.ctx.drawImage(
+                this.video, 0, 0, ...this.dimensions
+            );
+    }
+}
+
+register_provider({name: 'Webcam', description: 'Use your webcam!'});

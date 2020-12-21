bccfa8d53bdba9a6a06175c8ccb8decf20f4f4fc
Aneesh Durg <aneeshdurg17@gmail.com>
2020-12-21 09:59:05 -0800

    Add better webcam support

--- experiments/provider/webcam.js
+++ experiments/provider/webcam.js
@@ -3,22 +3,58 @@ class Webcam extends Provider {
         this.setup_ctx();
         this.video = document.createElement("video");
 
-        const constraints = {
-            video: {
-                mandatory: {
-                    maxWidth: 320,
-                    maxHeight: 180
-                }
-            }
-        }
-
+        let devices = undefined;
         try {
-            const stream = await navigator.mediaDevices.getUserMedia(constraints);
-            this.video.srcObject = stream;
-            this.video.play();
+            devices = await navigator.mediaDevices.enumerateDevices();
+            console.log(devices);
         } catch (err) {
             alert("Error initializing webcam!");
+            throw err;
         }
+
+        devices = devices.filter(d => d.kind === "videoinput");
+
+        this.container = document.createElement("div");
+        document.getElementById("ui").appendChild(this.container);
+
+        this.container.innerHTML = `<label for="webcamSelector">Choose a webcam: </label>`
+        const selector = document.createElement("select");
+        selector.id = "webcamSelector";
+        this.container.appendChild(selector);
+
+        devices.forEach(device => {
+            const entry = document.createElement("option");
+            entry.value = device.deviceId;
+            entry.innerHTML = device.label || device.deviceId.substr(0, 10);
+            selector.appendChild(entry)
+        });
+
+        const selectVideo = async () => {
+            const deviceId = selector.value;
+
+            const constraints = {
+                video: { deviceId: deviceId }
+            }
+
+            try {
+                if (this.video.srcObject) {
+                    const stream = this.video.srcObject;
+                    stream.getTracks().forEach(function(track) {
+                        track.stop();
+                    });
+                    this.video.srcObject = null;
+                }
+
+                const stream = await navigator.mediaDevices.getUserMedia(constraints);
+                this.video.srcObject = stream;
+                this.video.play();
+            } catch (err) {
+                alert("Error initializing webcam! " + err);
+                console.log(err);
+            }
+        }
+        selector.onchange = selectVideo;
+        selectVideo();
     }
 
     tick() {
